c5e2206ef60bdb1a65f1ba56f3b45fc7c8b85fc3
Symone Varnado <symonevarnado@gmail.com>
2020-05-03 22:16:20 -0500

    need to get geolocation to work with leaflet-react

--- client/index.js
+++ client/index.js
@@ -18,46 +18,6 @@ ReactDOM.render(
   document.getElementById('container')
 )
 
-// var mymap = L.map("map", {
-//   center: [41.8881084, -87.6320523],
-//   zoom: 13,
-// });
 
-// L.tileLayer(
-//   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-
-//   {
-//     attribution:
-//       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: "symoneav/ck93efi6e01o61imghlqsa45e",
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken:
-//       "pk.eyJ1Ijoic3ltb25lYXYiLCJhIjoiY2s3dXdndmpoMDR3MjNmcnk2dWV0eW9seSJ9.ZKSyDGUeOYPFUPYBnviS1Q",
-//   }
-// ).addTo(mymap);
-
-
-// var wfMarkers = [
-//   [41.87569, -87.63503],
-//   [41.92475, -87.70137],
-// ];
-// var wfMarker1 = L.marker(wfMarkers[0]).addTo(mymap);
-// wfMarker1
-//   .bindPopup("<b>We got the Booch!</b><br>Sage Lemongrass is in stock here!")
-//   .openPopup();
-
-// var wfMarker2 = L.marker(wfMarkers[1]).addTo(mymap);
-// wfMarker2
-//   .bindPopup("<b>We got the Booch!</b><br>Sage Lemongrass is in stock here!")
-//   .openPopup();
-
-
-// var marker = L.marker([41.8881084, -87.6320523]).addTo(mymap);
-
-// marker
-//   .bindPopup("<b>We got the Booch!</b><br>Sage Lemongrass is in stock here!")
-//   .openPopup();
 
 
